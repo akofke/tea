10bfde8763836f2087e55b2e31224ec3c7c32a42
Alex Kofke <akofke@gmail.com>
2015-04-26 17:25:40 -0400

    added decryption function, extracted feistel rounds to function
    Signed-off-by: Alex Kofke <akofke@gmail.com>

--- tea.s
+++ tea.s
@@ -7,7 +7,8 @@ newline: .asciiz "\n"
 
     .text
     .globl main
-main:
+_test_main:
+
     la $a0, key
     la $a1, value
 
@@ -37,60 +38,123 @@ tea_encrypt:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
 
-    lw $t0, 0($a1)      #v0
-    lw $t1, 4($a1)      #v1
+    addi $sp, $sp, -20
+    sw $s0, 0($sp)
+    sw $s1, 4($sp)
+    sw $s2, 8($sp)
+    sw $s3, 12($sp)
+    sw $s4, 16($sp)
+    sw $s5, 20($sp)
 
-    li $t2, 0           #sum
+    lw $s0, 0($a1)      #v0
+    lw $s1, 4($a1)      #v1
 
-    li $t9, 0x9e3779b9
+    # memory addresses of key and value array
+    move $s4, $a0
+    move $s5, $a1
 
-    li $t7, 32
+    li $s2, 0           #sum
 
-loop:
+    # li $t9, 0x9e3779b9
 
-    addu $t2, $t2, $t9
+    li $s3, 32          # loop index
+
+encrypt_loop:
+
+    # sum += delta
+    addiu $s2, $s2, 0x9e3779b9
+
+    move $a0, $s1       #v1 
+    lw $a1, 0($s4)      #k0
+    lw $a2, 4($s4)      #k1
+    move $a3, $s2
+
+    jal feistel_round
+    addu $s0, $s0, $v0
 
     # v0 += ((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1)
 
-    sll $t3, $t1, 4
-    lw $t5, 0($a0)      #k0
-    addu $t3, $t3, $t5
-
-    addu $t4, $t1, $t2
-
-    xor $t3, $t3, $t4
-
-    srl $t4, $t1, 5
-    lw $t5, 4($a0)      #k1
-    addu $t4, $t4, $t5
-
-    xor $t3, $t3, $t4
-    addu $t0, $t0, $t3
     ###
 
+    move $a0, $s0       #v0
+    lw $a1, 8($s4)      #k2
+    lw $a1, 12($s4)     #k3
+    move $a3, $s2
+
+    jal feistel_round
+    addu $s1, $s1, $v0
     # v1 += ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) +k3)
 
-    sll $t3, $t0, 4
-    lw $t5, 8($a0)
-    addu $t3, $t3, $t5
-
-    addu $t4, $t0, $t2
-
-    xor $t3, $t3, $t4
-
-    srl $t4, $t0, 5
-    lw $t5, 12($a0)
-    addu $t4, $t4, $t5
-
-    xor $t3, $t3, $t4
-    addu $t1, $t1, $t3
     ###
 
-    
-    
-    addi $t7, $t7, -1
-    bgtz $t7, loop
+    addi $s3, $s3, -1
+    bgtz $s3, encrypt_loop
     b done
+
+
+
+    .globl tea_decrypt
+tea_decrypt:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    addi $sp, $sp, -20
+    sw $s0, 0($sp)
+    sw $s1, 4($sp)
+    sw $s2, 8($sp)
+    sw $s3, 12($sp)
+    sw $s4, 16($sp)
+    sw $s5, 20($sp)
+
+    lw $s0, 0($a1)      #v0
+    lw $s1, 4($a1)      #v1
+
+    # memory addresses of key and value array
+    move $s4, $a0
+    move $s5, $a1
+
+    li $s2, 0xC6EF3720           #sum
+
+    li $s3, 32          # loop index
+
+decrypt_loop:
+    move $a0, $s0
+    lw $a1, 8($s4)
+    lw $a2, 12($s4)
+    move $a3, $s2
+
+    jal feistel_round
+    subu $s1, $s1, $v0
+
+    move $a0, $s1
+    lw $a1, 0($s4)
+    lw $a2, 4($s4)
+    move $a3, $s2
+
+    jal feistel_round
+    subu $s1, $s1, $v0
+
+    addi $s3, $s3, -1
+    bgtz $s3, decrypt_loop
+    b done
+
+    
+    
+
+feistel_round:
+    sll $t0, $a0, 4
+    addu $t0, $t0, $a1
+
+    addu $t1, $a0, $a3
+
+    xor $t0, $t0, $t1
+
+    srl $t1, $a0, 5
+    addu $t1, $t1, $a2
+
+    xor $v0, $t0, $t1
+    jr $ra
+
 
 done:
     sw $t0, 0($a1)
